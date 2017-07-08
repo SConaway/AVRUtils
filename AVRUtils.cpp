1c93e82259f0a3d8fa66fa8dea59f1682fb5cb16
Steven Conaway <steven.conaway@icloud.com>
2017-07-07 18:19:46 -0700

    Add More Boards to Library, restore keywords.txt

--- AVRUtils.cpp
+++ AVRUtils.cpp
@@ -20,7 +20,7 @@
 #include "AVRUtils.h"
 #include <Arduino.h>
 
-#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__) || (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_ATmega328P__)
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__) || defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__) || (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
 
 #else
     #error "AVRUtils does not support this processor. Please verify you have the right board and/or the right processor selected."
